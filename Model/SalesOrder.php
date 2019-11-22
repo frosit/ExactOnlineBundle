e6b93e86917d38ef73281608e603a00818f38a41
nils mechin <nils@zangra.com>
2019-11-22 11:01:45 +0100

    ZAN-1007 Remove undefined property LineNumber
    php-cs-fixer

--- Model/SalesOrder.php
+++ Model/SalesOrder.php
@@ -1,143 +1,141 @@
 <?php
 
-namespace aibianchi\ExactOnlineBundle\Model; 
+namespace aibianchi\ExactOnlineBundle\Model;
 
 use aibianchi\ExactOnlineBundle\Model\Base\Model;
+
 /**
- * Class SalesOrder extends Model
+ * Class SalesOrder extends Model.
  *
- * @package aibianchi\ExactOnlineBundle\Model
  * @see https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=SalesOrderSalesOrders
  *
- * @property string $OrderID Primary key
- * @property float $AmountDC Amount in the default currency of the company
- * @property float $AmountFC Amount in the currency of the transaction
- * @property int $ApprovalStatus Shows if this sales order is approved
- * @property string $ApprovalStatusDescription Description of ApprovalStatus
- * @property string $Approved Approval datetime
- * @property string $Approver User who approved the sales order
- * @property string $ApproverFullName Name of approver
- * @property string $Created Creation date
- * @property string $Creator User ID of creator
- * @property string $CreatorFullName Name of creator
- * @property string $Currency Currency code
- * @property string $DeliverTo Reference to delivery customer
- * @property string $DeliverToContactPerson Reference to contact person of delivery customer
- * @property string $DeliverToContactPersonFullName Name of contact person of delivery customer
- * @property string $DeliverToName Name of delivery customer
- * @property string $DeliveryAddress Delivery address
- * @property string $DeliveryDate Delivery date
- * @property int $DeliveryStatus Shipping status
- * @property string $DeliveryStatusDescription Description of DeliveryStatus
- * @property string $Description Description
- * @property int $Division Division code
- * @property string $Document Document that is manually linked to the sales order
- * @property int $DocumentNumber Number of the document
- * @property string $DocumentSubject Subject of the document
- * @property int $InvoiceStatus Invoice status
- * @property string $InvoiceStatusDescription Description of InvoiceStatus
- * @property string $InvoiceTo Reference to the Customer who will receive the invoice
- * @property string $InvoiceToContactPerson Reference to the Contact person of the customer who will receive the invoice
- * @property string $InvoiceToContactPersonFullName Name of the contact person of the customer who will receive the invoice
- * @property string $InvoiceToName Name of the customer who will receive the invoice
- * @property string $Modified Last modified date
- * @property string $Modifier User ID of modifier
- * @property string $ModifierFullName Name of modifier
- * @property string $OrderDate Order date
- * @property string $OrderedBy Customer who ordered the sales order
- * @property string $OrderedByContactPerson Contact person of the customer who ordered the sales order
- * @property string $OrderedByContactPersonFullName Name of contact person of the customer who ordered the sales order
- * @property string $OrderedByName Name of the customer who ordered the sales order
- * @property int $OrderNumber Number of sales order
- * @property string $PaymentCondition The payment condition used for due date and discount calculation
- * @property string $PaymentConditionDescription Description of PaymentCondition
- * @property string $PaymentReference Payment reference for sales order
- * @property string $Remarks Extra remarks
- * @property SalesOrderLines $SalesOrderLines Collection of lines
- * @property string $Salesperson Sales representative
- * @property string $SalespersonFullName Name of sales representative
- * @property string $ShippingMethod ShippingMethod
- * @property string $ShippingMethodDescription Description of ShippingMethod
- * @property int $Status The status of the sales order. 12 = Open, 20 = Partial, 21 = Complete, 45 = Cancelled.
- * @property string $StatusDescription Description of Status
- * @property string $TaxSchedule Tax schedule linked
- * @property string $TaxScheduleCode Code of the tax schedule
- * @property string $TaxScheduleDescription Description of the tax schedule
- * @property string $WarehouseCode Code of Warehouse
- * @property string $WarehouseDescription Description of Warehouse
- * @property string $WarehouseID Warehouse
- * @property string $YourRef The reference number of the customer
+ * @property string          $OrderID                        Primary key
+ * @property float           $AmountDC                       Amount in the default currency of the company
+ * @property float           $AmountFC                       Amount in the currency of the transaction
+ * @property int             $ApprovalStatus                 Shows if this sales order is approved
+ * @property string          $ApprovalStatusDescription      Description of ApprovalStatus
+ * @property string          $Approved                       Approval datetime
+ * @property string          $Approver                       User who approved the sales order
+ * @property string          $ApproverFullName               Name of approver
+ * @property string          $Created                        Creation date
+ * @property string          $Creator                        User ID of creator
+ * @property string          $CreatorFullName                Name of creator
+ * @property string          $Currency                       Currency code
+ * @property string          $DeliverTo                      Reference to delivery customer
+ * @property string          $DeliverToContactPerson         Reference to contact person of delivery customer
+ * @property string          $DeliverToContactPersonFullName Name of contact person of delivery customer
+ * @property string          $DeliverToName                  Name of delivery customer
+ * @property string          $DeliveryAddress                Delivery address
+ * @property string          $DeliveryDate                   Delivery date
+ * @property int             $DeliveryStatus                 Shipping status
+ * @property string          $DeliveryStatusDescription      Description of DeliveryStatus
+ * @property string          $Description                    Description
+ * @property int             $Division                       Division code
+ * @property string          $Document                       Document that is manually linked to the sales order
+ * @property int             $DocumentNumber                 Number of the document
+ * @property string          $DocumentSubject                Subject of the document
+ * @property int             $InvoiceStatus                  Invoice status
+ * @property string          $InvoiceStatusDescription       Description of InvoiceStatus
+ * @property string          $InvoiceTo                      Reference to the Customer who will receive the invoice
+ * @property string          $InvoiceToContactPerson         Reference to the Contact person of the customer who will receive the invoice
+ * @property string          $InvoiceToContactPersonFullName Name of the contact person of the customer who will receive the invoice
+ * @property string          $InvoiceToName                  Name of the customer who will receive the invoice
+ * @property string          $Modified                       Last modified date
+ * @property string          $Modifier                       User ID of modifier
+ * @property string          $ModifierFullName               Name of modifier
+ * @property string          $OrderDate                      Order date
+ * @property string          $OrderedBy                      Customer who ordered the sales order
+ * @property string          $OrderedByContactPerson         Contact person of the customer who ordered the sales order
+ * @property string          $OrderedByContactPersonFullName Name of contact person of the customer who ordered the sales order
+ * @property string          $OrderedByName                  Name of the customer who ordered the sales order
+ * @property int             $OrderNumber                    Number of sales order
+ * @property string          $PaymentCondition               The payment condition used for due date and discount calculation
+ * @property string          $PaymentConditionDescription    Description of PaymentCondition
+ * @property string          $PaymentReference               Payment reference for sales order
+ * @property string          $Remarks                        Extra remarks
+ * @property SalesOrderLines $SalesOrderLines                Collection of lines
+ * @property string          $Salesperson                    Sales representative
+ * @property string          $SalespersonFullName            Name of sales representative
+ * @property string          $ShippingMethod                 ShippingMethod
+ * @property string          $ShippingMethodDescription      Description of ShippingMethod
+ * @property int             $Status                         The status of the sales order. 12 = Open, 20 = Partial, 21 = Complete, 45 = Cancelled.
+ * @property string          $StatusDescription              Description of Status
+ * @property string          $TaxSchedule                    Tax schedule linked
+ * @property string          $TaxScheduleCode                Code of the tax schedule
+ * @property string          $TaxScheduleDescription         Description of the tax schedule
+ * @property string          $WarehouseCode                  Code of Warehouse
+ * @property string          $WarehouseDescription           Description of Warehouse
+ * @property string          $WarehouseID                    Warehouse
+ * @property string          $YourRef                        The reference number of the customer
  */
-class SalesOrder extends Model 
+class SalesOrder extends Model
 {
-        protected $primaryKey = 'OrderID';
-        protected $saleOrderLines;
-        protected $OrderID;
-        protected $AmountDC;
-        protected $AmountFC;
-        protected $ApprovalStatus;
-        protected $ApprovalStatusDescription;
-        protected $Approved;
-        protected $Approver;
-        protected $ApproverFullName;
-        protected $Created;
-        protected $Creator;
-        protected $CreatorFullName;
-        protected $Currency;
-        protected $DeliverTo;
-        protected $DeliverToContactPerson;
-        protected $DeliverToContactPersonFullName;
-        protected $DeliverToName;
-        protected $DeliveryAddress;
-        protected $DeliveryDate;
-        protected $DeliveryStatus;
-        protected $DeliveryStatusDescription;
-        protected $Description;
-        protected $Division;
-        protected $Document;
-        protected $DocumentNumber;
-        protected $DocumentSubject;
-        protected $InvoiceStatus;
-        protected $InvoiceStatusDescription;
-        protected $InvoiceTo;
-        protected $InvoiceToContactPerson;
-        protected $InvoiceToContactPersonFullName;
-        protected $InvoiceToName;
-        protected $Modified;
-        protected $Modifier;
-        protected $ModifierFullName;
-        protected $OrderDate;
-        protected $OrderedBy;
-        protected $OrderedByContactPerson;
-        protected $OrderedByContactPersonFullName;
-        protected $OrderedByName;
-        protected $OrderNumber;
-        protected $PaymentCondition;
-        protected $PaymentConditionDescription;
-        protected $PaymentReference;
-        protected $Remarks;
-        protected $SalesOrderLines;
-        protected $Salesperson;
-        protected $SalespersonFullName;
-        protected $ShippingMethod;
-        protected $ShippingMethodDescription;
-        protected $Status;
-        protected $StatusDescription;
-        protected $TaxSchedule;
-        protected $TaxScheduleCode;
-        protected $TaxScheduleDescription;
-        protected $WarehouseCode;
-        protected $WarehouseDescription;
-        protected $WarehouseID;
-        protected $YourRef;
-        protected $url = 'salesorder/SalesOrders';
+    protected $primaryKey = 'OrderID';
+    protected $saleOrderLines;
+    protected $OrderID;
+    protected $AmountDC;
+    protected $AmountFC;
+    protected $ApprovalStatus;
+    protected $ApprovalStatusDescription;
+    protected $Approved;
+    protected $Approver;
+    protected $ApproverFullName;
+    protected $Created;
+    protected $Creator;
+    protected $CreatorFullName;
+    protected $Currency;
+    protected $DeliverTo;
+    protected $DeliverToContactPerson;
+    protected $DeliverToContactPersonFullName;
+    protected $DeliverToName;
+    protected $DeliveryAddress;
+    protected $DeliveryDate;
+    protected $DeliveryStatus;
+    protected $DeliveryStatusDescription;
+    protected $Description;
+    protected $Division;
+    protected $Document;
+    protected $DocumentNumber;
+    protected $DocumentSubject;
+    protected $InvoiceStatus;
+    protected $InvoiceStatusDescription;
+    protected $InvoiceTo;
+    protected $InvoiceToContactPerson;
+    protected $InvoiceToContactPersonFullName;
+    protected $InvoiceToName;
+    protected $Modified;
+    protected $Modifier;
+    protected $ModifierFullName;
+    protected $OrderDate;
+    protected $OrderedBy;
+    protected $OrderedByContactPerson;
+    protected $OrderedByContactPersonFullName;
+    protected $OrderedByName;
+    protected $OrderNumber;
+    protected $PaymentCondition;
+    protected $PaymentConditionDescription;
+    protected $PaymentReference;
+    protected $Remarks;
+    protected $SalesOrderLines;
+    protected $Salesperson;
+    protected $SalespersonFullName;
+    protected $ShippingMethod;
+    protected $ShippingMethodDescription;
+    protected $Status;
+    protected $StatusDescription;
+    protected $TaxSchedule;
+    protected $TaxScheduleCode;
+    protected $TaxScheduleDescription;
+    protected $WarehouseCode;
+    protected $WarehouseDescription;
+    protected $WarehouseID;
+    protected $YourRef;
+    protected $url = 'salesorder/SalesOrders';
 
-    public function __construct(){
-
-        $this->SalesOrderLines = array();
-        $this->LineNumber= count($this->SalesOrderLines);
+    public function __construct()
+    {
+        $this->SalesOrderLines = [];
     }
-    
 
     /**
      * @return mixed
