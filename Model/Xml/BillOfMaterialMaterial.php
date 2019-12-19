c2a0c7557f54982ac6996fb6f8d44d8b6acdf3ec
nils mechin <nils@zangra.com>
2019-12-19 14:40:17 +0100

    ZAN-1008 Add XML Classes

---
+++ Model/Xml/BillOfMaterialMaterial.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace aibianchi\ExactOnlineBundle\Model\Xml;
+
+use aibianchi\ExactOnlineBundle\Model\BillOfMaterialMaterial as BaseItem;
+
+class BillOfMaterialMaterial extends BaseItem
+{
+    const TOPIC = 'BillOfMaterials';
+}
---
+++ Model/Xml/StockPosition.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace aibianchi\ExactOnlineBundle\Model\Xml;
+
+use aibianchi\ExactOnlineBundle\Model\BillOfMaterialMaterial as BaseItem;
+
+class StockPosition extends BaseItem
+{
+    const TOPIC = 'StockPositions';
+}
