0b821ba2ef82c5931187ad86d68a608901471119
nils mechin <nils@zangra.com>
2020-09-15 16:46:17 +0200

    Two new XML models

---
+++ Model/Xml/Account.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace aibianchi\ExactOnlineBundle\Model\Xml;
+
+
+class Account extends \aibianchi\ExactOnlineBundle\Model\Account
+{
+    const TOPIC = 'Accounts';
+}
---
+++ Model/Xml/Warehouse.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace aibianchi\ExactOnlineBundle\Model\Xml;
+
+
+class Warehouse extends \aibianchi\ExactOnlineBundle\Model\Warehouse
+{
+    const TOPIC = 'Warehouses';
+}
